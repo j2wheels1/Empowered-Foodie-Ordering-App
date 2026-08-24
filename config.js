3ebeafbbb45e746db15e26bb7d13ad1095312a3e
j2wheels1 <j2wheels@gmail.com>
2026-08-24 12:12:11 -0400

    Update ORDERS_ENDPOINT_URL in config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ window.EF_CONFIG = {
   // 2) ORDER SUBMISSIONS
   // Paste the Web App URL you get after deploying the Apps Script in
   // /apps-script/Code.gs. This is where order requests get saved.
-  ORDERS_ENDPOINT_URL: "https://script.google.com/macros/s/AKfycbyZdV4z8s_aYWFbEXikxe2MvXtVqzhaUeWRP6e-i_UtNKxBVu6blyhDDidlVigrql4B/exec",
+  ORDERS_ENDPOINT_URL: "https://script.google.com/macros/s/AKfycbyPnnPYGw4h59bdAJPDxOEIfWfgNCdInVM0bDtbZPnVDJ0_ONHvxBZuGPxHSRjTjnhN/exec",
 
   // Business info shown in the footer / confirmation message.
   BUSINESS_NAME: "Empowered Foodie",
